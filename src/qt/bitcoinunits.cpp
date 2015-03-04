5a40a6a7ad8932b5969ee7e3624fb6e18935cb07
Clyde Datastruct <beetlemon.cwts@gmail.com>
2015-03-04 19:21:13 +0800

    Needed changes

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -49,9 +49,9 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("Litecoins");
-    case mBTC: return QString("Milli-Litecoins (1 / 1,000)");
-    case uBTC: return QString("Micro-Litecoins (1 / 1,000,000)");
+    case BTC: return QString("Rodentcoins");
+    case mBTC: return QString("Milli-Rodentcoins (1 / 1,000)");
+    case uBTC: return QString("Micro-Rodentcoins (1 / 1,000,000)");
     default: return QString("???");
     }
 }
